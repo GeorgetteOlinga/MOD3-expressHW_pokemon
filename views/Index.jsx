81c29438aed7aadc72aa01dfec17bea2081bfe94
git <olingageorgette1@gmail.com>
2023-12-09 06:38:21 -0500

    Added dynamic anchor tags to index.jsx

--- views/Index.jsx
+++ views/Index.jsx
@@ -13,9 +13,12 @@ function Index({ pokemon }) {
         <>
             <h1 style={h1Style}>See All The Pokemon!</h1>
             <ul id="pokemon-list">
-                {pokemon.map(p => (
+                {pokemon.map((p, i) => (
                     <li key={p.name}>
                         <h3>{capitalize(p.name)}</h3>
+                        <a href={`/pokemon/${i}`}>
+                            Go to pokemon's page &#9658;
+                        </a><br />
                         <img src={p.img} alt={`Picture of ${p.name}`} />
                     </li>
                 ))}
