7f45c89710691edcf5d8562a2186b2cbfaf0a5b3
git <olingageorgette1@gmail.com>
2023-12-09 06:14:51 -0500

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+/*
+    / = root folder
+    ./ = current folder
+    ../ = back a folder
+*/
+const pokemon = require("./models/pokemon.js")
 
 app.get('/', (req,res)=>{
     res.send("Welcome to Pokemon App!")
@@ -11,7 +17,9 @@ app.get('/', (req,res)=>{
     function(){}
     ()=>{}
 */
-
+app.get('/pokemon', (req,res)=>{
+    res.send(pokemon)
+})
 
 
 
